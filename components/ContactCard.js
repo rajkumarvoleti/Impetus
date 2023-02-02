a04b78c52eb4001965732f0f353270a4ee69b7a1
Utsab Kundu <92643166+Utsab5@users.noreply.github.com>
2023-02-02 18:43:04 +0530

    Update ContactCard.js

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -1,12 +1,16 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import Image from "next/image";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import IconButton from "@mui/material/IconButton";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const styles = {
   height: "400px",
   width: "400px",
   img: {
-    margin: "20px",
+    margin: "10px",
   },
   "div h4": {
     margin: "10px",
@@ -15,6 +19,7 @@ const styles = {
     display: "flex !important",
     flexDirection: "column",
   },
+  
   display: "flex !important",
   flexDirection: "column",
   justifyContent: "center",
@@ -24,9 +29,10 @@ const styles = {
 
 export default function ContactCard() {
   return (
-    <Box sx={styles}>
-      <Image src="/images/guy.png" alt="guy" height="200" width="200" />
-      <Box>
+    <Box sx={styles} className="pcard">
+      <Image className="profile" src="/images/guy.png" alt="guy" height="200" width="200" />
+      
+      <Box className="title">
         <Typography variant="h3" fontSize="25px">
           Priyanshu Choudary
         </Typography>
@@ -40,6 +46,16 @@ export default function ContactCard() {
           +91 9799054122
         </Typography>
       </Box>
+
+      <Box className="text">
+        
+        <a href="#"><FacebookIcon /></a>
+        <a href="#"><InstagramIcon /></a>
+        <a href="#"><LinkedInIcon /></a>
+        
+        
+      </Box>
+
     </Box>
   );
 }
